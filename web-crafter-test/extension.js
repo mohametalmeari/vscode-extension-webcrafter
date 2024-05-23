a58e097370a882ca828eb2d09ada43655624325e
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-24 02:13:07 +0300

    Add new commands for web-crafter-test extension

--- web-crafter-test/extension.js
+++ web-crafter-test/extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,28 +9,43 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log("Test");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "web-crafter-test" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "web-crafter-test.test-command",
+    function () {
+      // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('web-crafter-test.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      console.log("Test Command");
+      // Display a message box to the user
+      vscode.window.showInformationMessage(
+        "Hello World from Web Crafter Test!"
+      );
+    }
+  );
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Web Crafter Test!');
-	});
+  context.subscriptions.push(disposable);
 
-	context.subscriptions.push(disposable);
+  // Add a new command
+  let newCommand = vscode.commands.registerCommand(
+    "web-crafter-test.new-command",
+    function () {
+      console.log("New Command is called");
+      vscode.window.showInformationMessage("New Command");
+    }
+  );
+  context.subscriptions.push(newCommand);
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
